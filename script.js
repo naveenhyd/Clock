19a6a3aeea9819c1cedf52d483b706d8904e2840
Naveen Padigala <n_padigala@apple.com>
2020-01-30 19:05:15 +0530

    css clock added

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const SecondHand = document.querySelector('[data-second-hand]');
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRation = (minutesRatio + currentDate.getHours()) / 12;
+
+    setRotation(SecondHand,secondsRatio);
+    setRotation(minuteHand,minutesRatio);
+    setRotation(hourHand,hoursRation);
+}
+
+function setRotation(element, rotationRation) {
+    element.style.setProperty('--rotation', rotationRation * 360);
+}
+
+setClock();
